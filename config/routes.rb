aa282196b9b593d8b4af4f0188e0d6dcc4311ece
= <=>
2017-01-25 15:17:47 -0800

    Make home page route to categories#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'categories#index'
 
   resources :categories do 
     resources :recipes 
@@ -7,13 +8,12 @@ Rails.application.routes.draw do
 
   resources :ingredients
 
-  
-  get '/' => 'category#index'
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  
+
 end
