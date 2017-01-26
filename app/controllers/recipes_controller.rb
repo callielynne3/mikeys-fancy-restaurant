8115a37342a9df0e383e6ec58d27a08a23ca20ea
Derek Pham <derekmpham@gmail.com>
2017-01-25 16:37:24 -0800

    Create edit feature--add things to controller and create new edit page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,6 +11,8 @@ class RecipesController < ApplicationController
   end
 
   def edit
+    @category = Category.find(params[:category_id])
+    @recipe = Recipe.find(params[:id])
   end
 
   def create
@@ -27,6 +29,17 @@ class RecipesController < ApplicationController
   end
 
   def update
+    @category = Category.find(params[:category_id])
+    @recipe = Recipe.find(params[:id])
+    respond_to do |format|
+      if @recipe.update(recipe_params)
+        format.html { redirect_to category_recipe_path(@category, @recipe), notice: 'Recipe successfully updated' }
+        format.json { render :show, status: :ok, location: @recipe }
+      else
+        format.html { render :edit }
+        format.json { render @recipe.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
