270b8a5e50476a12b3697da949d25256529d6d19
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2017-01-26 23:16:50 -0800

    :construction: Formatting updates

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,15 +2,15 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    @appetizers = Category.find_by_name('Appetizer').recipes.sort_by(rate)
+    @appetizers = Category.find_by_name('Appetizer').recipes#.sort_by(rate)
     @salads = Category.find_by_name('Salad').recipes
     @mains = Category.find_by_name('Main Course').recipes
     @desserts = Category.find_by_name('Dessert').recipes
-  end 
-
-  def show 
-    @category = Category.find(params[:id])
-    @recipe = @category.recipes
   end
-  
+
+  def show
+    @category = Category.find(params[:id])
+    @recipes = @category.recipes
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Recipe.destroy_all
 Ingredient.destroy_all
 Rating.destroy_all
 
-3.times do
+10.times do
   User.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "p123")
 end
 
@@ -21,14 +21,19 @@ categories = Category.all
 difficulty = ["Easy", "Medium", "Hard"]
 prep_time = [15, 20, 30, 35, 45, 50, 60, 75, 90, 120, 200]
 
-categories.each do |category|
-  category.recipes.create!(name: Faker::Pokemon.name, difficulty: difficulty.sample, prep_time: prep_time.sample, directions: Faker::Lorem.paragraph, creator: Faker::StarWars.character)
+rand(5..15).times do
+  categories.each do |category|
+    category.recipes.create!(name: Faker::Pokemon.name, difficulty: difficulty.sample, prep_time: prep_time.sample, directions: Faker::Lorem.paragraph, creator: Faker::StarWars.character)
+  end
 end
 
 recipes = Recipe.all
 
 recipes.each do |recipe|
-  5.times do
+  rand(0..7).times do
+    recipe.ingredients.create!(name: "#{Faker::Food.measurement} #{Faker::Food.spice}")
+  end
+  rand(0..7).times do
     recipe.ingredients.create!(name: Faker::Food.ingredient)
   end
   recipe.ratings.create!(user_id: users.first.id, rate: rand(1..5))
@@ -37,7 +42,7 @@ end
 users = User.all
 
 users.each do |user|
-  user.recipes << Recipe.first
+  user.recipes << Recipe.all.sample
 end
 
 ratings = Rating.all
