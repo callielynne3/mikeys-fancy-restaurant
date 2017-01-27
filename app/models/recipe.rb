16651398c60ec522fc6aaa02d5132e1cb06ad311
Jonah Price <email": null>
2017-01-27 15:28:44 -0800

    Minor fixes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,11 +11,15 @@ class Recipe < ApplicationRecord
   validates :name, :difficulty, :prep_time, :directions, :creator, presence: true
 
   def average_rating
-    sum = self.ratings.reduce(0) { |rating_total, rating| rating_total + rating.rate }
-    sum / self.ratings.count
+    if self.ratings.count > 0
+      sum = self.ratings.reduce(0) { |rating_total, rating| rating_total + rating.rate }
+      sum / self.ratings.count
+    else
+      "Not yet rated. :("
+    end
   end
 
-  def self.search(search) where("name ILIKE ? OR directions ILIKE ?", "%#{search}%", "%#{search}%") 
+  def self.search(search) where("name ILIKE ? OR directions ILIKE ?", "%#{search}%", "%#{search}%")
   end
 
 end
