8960348a3b11845cd2332475c4a551d1041c46f7
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2017-01-27 00:33:56 -0800

    :seedling: Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,41 +8,36 @@ Rating.destroy_all
   User.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "p123")
 end
 
-users = User.all
-
 categories = ["Appetizer", "Salad", "Main Course", "Dessert"]
 
 categories.each do |category|
   Category.create!(name: category)
 end
 
-categories = Category.all
-
 difficulty = ["Easy", "Medium", "Hard"]
 prep_time = [15, 20, 30, 35, 45, 50, 60, 75, 90, 120, 200]
 
-categories.each do |category|
+Category.all.each do |category|
   rand(5..15).times do
     category.recipes.create!(name: Faker::Pokemon.name, difficulty: difficulty.sample, prep_time: prep_time.sample, directions: Faker::Lorem.paragraph, creator: Faker::StarWars.character)
   end
 end
 
-recipes = Recipe.all
-
-recipes.each do |recipe|
+Recipe.all.each do |recipe|
   rand(0..7).times do
     recipe.ingredients.create!(name: "#{Faker::Food.measurement} #{Faker::Food.spice}")
   end
   rand(0..7).times do
     recipe.ingredients.create!(name: Faker::Food.ingredient)
   end
-  recipe.ratings.create!(user_id: users.first.id, rate: rand(1..5))
 end
 
-users = User.all
-
-users.each do |user|
-  user.recipes << Recipe.all.sample
+User.all.each do |user|
+  p user.ratings.length
+  Recipe.all.each do |recipe|
+    p recipe.ratings.length
+    recipe.ratings.create!(user_id: user.id, rate: rand(1..5))
+  end
 end
 
 ratings = Rating.all
