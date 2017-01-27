4a326efe0e601c0246ce19942bb5644102bc5591
= <=>
2017-01-27 11:51:32 -0800

    :mag: Implement a search bar for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,12 @@ class Recipe < ApplicationRecord
   has_many :ingredients_recipes
   has_many :ratings
 
+
   accepts_nested_attributes_for :ingredients
 
   validates :name, :difficulty, :prep_time, :directions, :creator, presence: true
+
+  def self.search(search) where("name ILIKE ? OR directions ILIKE ?", "%#{search}%", "%#{search}%") 
+  end
+
 end
